df86eaa4933023ef2af871d58795808796936199
Srikeshram <110611485+srikesh-07@users.noreply.github.com>
2023-09-28 00:01:00 +0530

    Update mixup_v2.py

--- mixup_v2.py
+++ mixup_v2.py
@@ -137,8 +137,8 @@ def get_args():
                         help='name of dataset (default: PTC)')
     parser.add_argument('--batch_size', type=int, default=32,
                         help='input batch size for training (default: 32)')
-    parser.add_argument('--epochs', type=int, default=300,
-                        help='number of epochs to train (default: 500)')
+    parser.add_argument('--epochs', type=int, default=100,
+                        help='number of epochs to train (default: 100)')
     parser.add_argument('--lr', type=float, default=0.01,
                         help='learning rate (default: 0.01)')
     parser.add_argument('--seed', type=int, default=0,
